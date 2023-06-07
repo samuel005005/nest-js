32f1e5e75d295c04ac61e8d998221b6d6b4f448a
Samuel Paez Perez <spaez@viva.com.do>
2023-06-07 15:56:17 -0400

    first commit

---
+++ car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla'
+        }
+    ]
+
+    findAll() {
+        return this.cars;
+    }
+
+    findOneById(id: number) {
+
+        const car = this.cars.find(car => car.id == id);
+
+        if (!car) { 
+            throw new NotFoundException(`Car with id '${id} not found'`)
+        }
+
+        return car;
+    }
+}
